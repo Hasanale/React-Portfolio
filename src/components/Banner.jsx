de4680362172a353a939990b900d1574402ccc0d
HSN <choudhryhassan37@gmail.com>
2024-08-26 10:42:34 +0500

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,67 @@
 import React from "react";
+import Image from "../assets/avatar.svg";
+import { FaGithub, FaFacebook, FaLinkedin } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 
 function Banner() {
   return (
-    <div className="section" id="home">
-      Banner
-    </div>
+    <section className="min-h-[85vh] lg:min-h-[78vh] items-center" id="home">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12 ">
+          {/* text */}
+          <div className="flex-1 text-center font-secondary lg:text-left ">
+            <h1 className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
+              Hassan <span>Ali</span>
+            </h1>
+            <div className="mb-6 text-[36px] lg:text-[60px] font-secondary uppercase font-semibold leading-[1]">
+              <span className="text-white mr-4">I am a </span>
+              <TypeAnimation
+                sequence={[
+                  "Web Developer",
+                  2000,
+                  "Freelancer",
+                  2000,
+                  "Designer",
+                  2000,
+                ]}
+                speed={250}
+                repeat={Infinity}
+                wrapper="span"
+                className="text-accent"
+              />
+            </div>
+            <p className="mb-8 mx-w-lg mx-auto lg:max-0 ">
+              I'm a dedicated web developer with a passion for crafting
+              responsive, user-friendly, and visually engaging websites. With 1
+              year of experience, I specialize in HTML, CSS, JavaScript, and
+              modern frameworks like React and Tailwind CSS.
+            </p>
+            <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </div>
+            <div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+              <a href="#">
+                <FaFacebook />
+              </a>
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaLinkedin />
+              </a>
+            </div>
+          </div>
+          {/* image */}
+          <div className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] ">
+            {/* <img src={Image} alt="" /> */}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
 function Header() {
-  return <div className="bg-pink-300">Header</div>;
+  return (
+    <header className=" py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <a href="#" className="text-gradient text-4xl font-bold">
+            HA
+          </a>
+          <button className="btn btn-sm">Work with me</button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
