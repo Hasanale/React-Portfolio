34f9f0e71961bfc6dad4b0a43fb8cfb948169a9a
HSN <choudhryhassan37@gmail.com>
2024-08-26 13:20:35 +0500

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,79 @@
+import React from "react";
+import { useInView } from "react-intersection-observer";
+import CountUp from "react-countup";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
 function About() {
+  const [ref, inView] = useInView({ threshold: 0.5 });
   return (
-    <div className="section" id="about">
-      About
-    </div>
+    <section className="section" id="about" ref={ref}>
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-15 lg:gap-y-0 h-screen">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            img
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            <h2 className="h2 text-accent">About Me</h2>
+            <h3 className="h3 mb-4">I'm a dedicated web developer.</h3>
+            <p className="mb-6">
+              I'm a dedicated web developer with a passion for crafting
+              responsive, user-friendly, and visually engaging websites. With 1
+              year of experience, I specialize in HTML, CSS, JavaScript, and
+              modern frameworks like React and Tailwind CSS.
+            </p>
+            {/* stats */}
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12">
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient  mb-2  ">
+                  {inView ? <CountUp start={0} end={3} duration={3} /> : null}+
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Years of <br />
+                  Experience
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient  mb-2  ">
+                  {inView ? <CountUp start={0} end={15} duration={3} /> : null}+
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Projects <br />
+                  Completed
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient  mb-2  ">
+                  {inView ? <CountUp start={0} end={10} duration={3} /> : null}+
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Satisfied <br />
+                  Clients
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,6 +3,7 @@ import Image from "../assets/avatar.svg";
 import { FaGithub, FaFacebook, FaLinkedin } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 function Banner() {
   return (
@@ -11,10 +12,22 @@ function Banner() {
         <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12 ">
           {/* text */}
           <div className="flex-1 text-center font-secondary lg:text-left ">
-            <h1 className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[0.8] lg:text-[110px]"
+            >
               Hassan <span>Ali</span>
-            </h1>
-            <div className="mb-6 text-[36px] lg:text-[60px] font-secondary uppercase font-semibold leading-[1]">
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[60px] font-secondary uppercase font-semibold leading-[1]"
+            >
               <span className="text-white mr-4">I am a </span>
               <TypeAnimation
                 sequence={[
@@ -30,20 +43,38 @@ function Banner() {
                 wrapper="span"
                 className="text-accent"
               />
-            </div>
-            <p className="mb-8 mx-w-lg mx-auto lg:max-0 ">
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 mx-w-lg mx-auto lg:max-0 "
+            >
               I'm a dedicated web developer with a passion for crafting
               responsive, user-friendly, and visually engaging websites. With 1
               year of experience, I specialize in HTML, CSS, JavaScript, and
               modern frameworks like React and Tailwind CSS.
-            </p>
-            <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
               <button className="btn btn-lg">Contact me</button>
               <a href="#" className="text-gradient btn-link">
                 My Portfolio
               </a>
-            </div>
-            <div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+            </motion.div>
+            <motion.div
+              // variants={fadeIn("up", 0.7)}
+              // initial="hidden"
+              // whileInView={"show"}
+              // viewport={{ once: false, amount: 0.7 }}
+              className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0"
+            >
               <a href="#">
                 <FaFacebook />
               </a>
@@ -53,12 +84,17 @@ function Banner() {
               <a href="#">
                 <FaLinkedin />
               </a>
-            </div>
+            </motion.div>
           </div>
           {/* image */}
-          <div className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] ">
+          <motion.div
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] "
+          >
             {/* <img src={Image} alt="" /> */}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
